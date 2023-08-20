c34df66e82d39be3e0ff2699fc62b70a5debccd8
zazimov <zalmanazimov@pursuit.org>
2023-08-20 16:31:36 -0400

    added PG_PASSWORD to DBConfig

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -6,7 +6,7 @@ const cn = {
   database: process.env.PG_DATABASE,
   port: process.env.PG_PORT,
   user: process.env.PG_USER,
-  //   password: process.env.PG_PASSWORD,
+  password: process.env.PG_PASSWORD,
   // When testing, comment out 'password' key, but keep in 'port' key. When Deploying, comment out 'port' key, and uncomment 'password' key so that it is active
 };
 
